f573bb4534293a6484d50f02f6e84398a230b850
kognitivamitsuo <mitsuo.kaneko@yahoo.com.br>
2025-07-08 11:44:39 -0300

    Delete docs directory

--- docs/utils/uiRenderer.js
+++
@@ -1,9 +0,0 @@
-function renderizarMensagem(origem, mensagem) {
-  const chat = document.getElementById("chatMessages");
-  const msg = document.createElement("div");
-  msg.className = origem === "user" ? "chat-message user" : "chat-message ai";
-  msg.innerText = mensagem;
-  chat.appendChild(msg);
-  chat.scrollTop = chat.scrollHeight;
-}
-export { renderizarMensagem };
