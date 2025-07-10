655f835bef6223ced21e9d49d374fe4346bd767d
kognitivamitsuo <mitsuo.kaneko@yahoo.com.br>
2025-07-10 19:59:04 -0300

    Delete js directory

--- js/auth.js
+++
@@ -1,9 +0,0 @@
-function verificarTokenJWT() {
-    const token = localStorage.getItem('jwt_token');
-    if (!token) {
-        window.location.href = '/login';
-        return false;
-    }
-    console.log('Token JWT encontrado e válido.');
-    return true;
-}
--- js/contextService.js
+++
@@ -1,7 +0,0 @@
-function salvarContextoNoLocalStorage(contexto) {
-    localStorage.setItem('contexto_conversa', JSON.stringify(contexto));
-}
-function recuperarContextoDoLocalStorage() {
-    const contexto = localStorage.getItem('contexto_conversa');
-    return contexto ? JSON.parse(contexto) : null;
-}
